d7d8ba4f28112c236fec8c62f09c82be1ef5f59b
4tellA223 <estella.hezheng@gmail.com>
2024-09-10 12:30:30 -0700

    Add files via upload

---
+++ practice1/HelloWorld.java
@@ -0,0 +1,12 @@
+/*
+ * Ap Computer Science
+ * Our First Example
+ * Estella Hezheng
+ */
+public class HelloWorld{
+    public static void main(String[] args){
+        //output some text
+        System.out.println("Hello World!");
+    }
+    
+}
