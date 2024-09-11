7dbf2812c8cf5f41283800d7cd7fc58b606304ab
4tellA223 <estella.hezheng@gmail.com>
2024-09-11 13:30:11 -0700

    Delete practice1 directory

--- practice1/Haiku.java
+++
@@ -1,8 +0,0 @@
-public class Haiku{
-    public static void main(String[] args){
-        System.out.println("You and me alone");
-        System.out.println("Madness of world locked away");
-        System.out.println("Peace and quiet reigns");
-        
-    }
-}
