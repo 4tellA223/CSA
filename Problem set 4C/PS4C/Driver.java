d6fbc8d73880ad3c401972f90537b876ec48e77d
4tellA223 <estella.hezheng@gmail.com>
2024-10-30 14:42:52 -0700

    Problem set 4C

---
+++ Problem set 4C/PS4C/Driver.java
@@ -0,0 +1,9 @@
+public class Driver{
+
+    public static void main(String []arg){
+        //total sentiment
+        System.out.println(Review.totalSentiment("SimpleReview.txt"));
+        //star Rating
+        System.out.println(Review.starRating("SimpleReview.txt"));
+    }
+}
